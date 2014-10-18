760b37b7ec77f6f91f57a2989a03aca6c5abca6e
padarshr <padarshr@codeaurora.org>
2014-10-17 19:38:10 -0700

    Remove vFb parameter from CMDLINE
    This change is to remove the virtual fb
    parameter form kernel command line,
    as it is no longer needed.
    
    Change-Id: I8aff1130042f21ba7dcc959ce74731c96a50ee09

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -76,7 +76,7 @@ TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_PERSISTIMAGE_FILE_SYSTEM_TYPE := ext4
 
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci video=vfb:640x400,bpp=32,memsize=3072000 lpm_levels.sleep_disabled=1 earlyprintk
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk
 BOARD_KERNEL_SEPARATED_DT := true
 
 BOARD_EGL_CFG := device/qcom/msm8909/egl.cfg
