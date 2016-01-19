143b33397981235bb9184fdd8a6a2ed0a1abfaca
Konsta <konsta09@gmail.com>
2016-01-19 21:03:07 +0200

    restructure and clean up
    Signed-off-by: Misha Komarovskiy <zombah@gmail.com>
    
    Conflicts:
    	prebuilt/system/etc/firmware/wlan/prima/WCNSS_cfg.dat
    	prebuilt/system/etc/thermal-engine-8909.conf
    	prebuilt/system/usr/keylayout/FT5x36.kl
    	prebuilt/system/usr/keylayout/msg2xxx.kl
    	ramdisk/init.qcom.serialno.sh

--- msm8909.mk
+++ msm8909.mk
@@ -13,12 +13,6 @@ endif #TARGET_USES_QCOM_BSP
 #QTIC flag
 -include $(QCPATH)/common/config/qtic-config.mk
 
-# media_profiles and media_codecs xmls for msm8909
-ifeq ($(TARGET_ENABLE_QC_AV_ENHANCEMENTS), true)
-PRODUCT_COPY_FILES += device/qcom/msm8909/media/media_profiles_8909.xml:system/etc/media_profiles.xml \
-                      device/qcom/msm8909/media/media_codecs_8909.xml:system/etc/media_codecs.xml
-endif
-
 $(call inherit-product, device/qcom/common/common.mk)
 
 PRODUCT_NAME := msm8909
@@ -34,20 +28,6 @@ endif
 
 #Android EGL implementation
 PRODUCT_PACKAGES += libGLES_android
-
-# Audio configuration file
-PRODUCT_COPY_FILES += \
-    device/qcom/msm8909/audio_policy.conf:system/etc/audio_policy.conf \
-    device/qcom/msm8909/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-    device/qcom/msm8909/mixer_paths_qrd_skuh.xml:system/etc/mixer_paths_qrd_skuh.xml \
-    device/qcom/msm8909/mixer_paths_qrd_skui.xml:system/etc/mixer_paths_qrd_skui.xml \
-    device/qcom/msm8909/mixer_paths.xml:system/etc/mixer_paths.xml \
-    device/qcom/msm8909/mixer_paths_msm8909_pm8916.xml:system/etc/mixer_paths_msm8909_pm8916.xml \
-    device/qcom/msm8909/mixer_paths_skua.xml:system/etc/mixer_paths_skua.xml \
-    device/qcom/msm8909/mixer_paths_skuc.xml:system/etc/mixer_paths_skuc.xml \
-    device/qcom/msm8909/mixer_paths_skue.xml:system/etc/mixer_paths_skue.xml \
-    device/qcom/msm8909/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
-    device/qcom/msm8909/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml
 
 # NFC packages
 ifeq ($(TARGET_USES_QCA_NFC),true)
@@ -97,10 +77,6 @@ PRODUCT_BOOT_JARS += qcmediaplayer \
                      vcard \
                      tcmiface
 
-# Listen configuration file
-PRODUCT_COPY_FILES += \
-    device/qcom/msm8909/listen_platform_info.xml:system/etc/listen_platform_info.xml
-
 # Feature definition files for msm8909
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
@@ -108,9 +84,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
     frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
     frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml
-
-#fstab.qcom
-PRODUCT_PACKAGES += fstab.qcom
 
 PRODUCT_PACKAGES += \
     libqcomvisualizer \
@@ -124,15 +97,6 @@ PRODUCT_PACKAGES += libSubSystemShutdown
 
 PRODUCT_PACKAGES += wcnss_service
 
-#wlan driver
-PRODUCT_COPY_FILES += \
-    device/qcom/msm8909/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
-    device/qcom/msm8909/WCNSS_wlan_dictionary.dat:persist/WCNSS_wlan_dictionary.dat \
-    device/qcom/msm8909/WCNSS_qcom_wlan_nv.bin:persist/WCNSS_qcom_wlan_nv.bin
-
-PRODUCT_PACKAGES += \
-    wpa_supplicant_overlay.conf \
-    p2p_supplicant_overlay.conf
 #ANT+ stack
 PRODUCT_PACKAGES += \
 AntHalService \
