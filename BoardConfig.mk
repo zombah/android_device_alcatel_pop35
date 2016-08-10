1bf0b8c5afd1299719fe9be17e52fcdf181acdb5
Konsta <konsta09@gmail.com>
2016-08-10 10:42:04 +0300

    remove libinit_msm
    * http://review.cyanogenmod.org/#/c/144008/
    
    Change-Id: Ic5137953e39ae08c6e9b781d23b9572d6e020666

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -88,7 +88,6 @@ USE_OPENGL_RENDERER := true
 #BOARD_HARDWARE_CLASS := $(LOCAL_PATH)/cmhw/
 
 # Init
-TARGET_INIT_VENDOR_LIB := libinit_msm
 TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
 
 # Lights
