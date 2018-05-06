d4e7692adaac36b6cc60d10a6f053b13099990b6
Misha Komarovskiy <zombah@gmail.com>
2018-05-06 15:37:18 +0300

    selinux: Set permissive for now
    Signed-off-by: Misha Komarovskiy <zombah@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,7 +38,7 @@ TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a7 -mfpu=neon-vfpv4 -mfloat-abi=softfp
 BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk androidboot.bootloader=L2A73030BR00
-BOARD_KERNEL_CMDLINE += selinux=1 enforcing=1
+BOARD_KERNEL_CMDLINE += selinux=1 enforcing=0 androidboot.selinux=permissive
 BOARD_RAMDISK_OFFSET := 0x01000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
